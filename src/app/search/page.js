18dd9913c533a37c082c21e9e7e1bf8da59eb30f
Brian Lin <brianlin7453@gmail.com>
2024-04-02 19:47:00 -0400

    commit message

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -17,24 +17,33 @@ import environment from "../../../environment";
 function FormComponent() {
   const [barcode, setBarcode] = useState("");
   const [itemList, setItemList] = useState([]);
-
+  function getBarcodeItem(event) {
+    event.preventDefault();
+    axios.get(environment.API_URL + `/item?barcode=${barcode}`).then((res) => {
+      const message = res.data.message;
+      if (message === "No entry found.") {
+        const noObjectFound = {
+          manufacturer: "No Item Found",
+          type: "No Item Found",
+          volume: "No Item Found",
+          location: "No Item Found",
+          description: "No Item Found",
+          price: "No Item Found",
+        };
+        setItemList([...itemList, noObjectFound]);
+      } else {
+        setItemList([...itemList, res.data.data]);
+      }
+      setBarcode("");
+    });
+  }
   return (
     <div className="flex bg-pink-100 min-h-screen h-1/4 w-full">
       <SideBar />
       <div className="pl-16 pt-16 pr-16 w-full">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            axios
-              .get(environment.API_URL + `/item?barcode=${barcode}`)
-              .then(function (response) {
-                setBarcode("");
-                setItemList([...itemList, response.data.data]);
-                console.log(itemList);
-              });
-          }}
-        >
+        <form onSubmit={getBarcodeItem}>
           <Input
+            autoFocus
             value={barcode}
             onChange={(event) => setBarcode(event.target.value)}
             className="w-2/3 border-black"
